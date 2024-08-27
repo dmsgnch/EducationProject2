962373c9fe7cbca3fb9f8937bd90b0cdb74920a6
Dmytro <dimchikdg89@gmail.com>
2024-08-27 13:54:20 +0300

    Initial commit

---
+++ EducationProject2/Views/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace EducationProject2
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
